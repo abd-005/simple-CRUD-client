d1984b75180a1cd69525d9d12c53fbcbdccbc0ee
abd-005 <abd.me7@gmail.com>
2025-11-01 18:20:31 +0600

    fecth n show data to UI

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { use, useState } from "react";
 
-const Users = () => {
+const Users = ({ usersPromise }) => {
+  const initialData = use(usersPromise);
+  const [users, setUsers] = useState(initialData);
+
   const handleAddUser = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const email = e.target.email.value;
     // console.log(name, email);
-    const newUser = {name,email}
+    const newUser = { name, email };
 
     // save this user data to the database
-    fetch("http://localhost:3000/users",{
-        method: 'POST',
-        headers:{
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(newUser),
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
     })
       .then((res) => res.json())
-      .then((data) => {console.log('after post:', {data})
-    if(data.insertedId){
-        console.log('user added successfully');
-        e.target.reset;
-    }});
+      .then((data) => {
+        console.log("after post:", { data });
+        if (data.insertedId) {
+          console.log("user added successfully");
+          newUser._id = data.insertedId
+          const newUsers = [...users, newUser];
+          setUsers(newUsers);
+          e.target.reset;
+        }
+      });
   };
 
   return (
@@ -33,6 +41,17 @@ const Users = () => {
         <br />
         <input type="submit" value="Add User" />
       </form>
+
+      <p>____________________________</p>
+      <div>
+        {
+        users.map(user => (
+          <p key={user._id}>
+            {user.name} : {user.email}
+          </p>
+        ))
+        }
+      </div>
     </div>
   );
 };
