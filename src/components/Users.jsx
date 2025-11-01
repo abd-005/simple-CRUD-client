f56ff0b6bd87bb1d4e41356af6722fc846f60cac
abd-005 <abd.me7@gmail.com>
2025-11-01 17:18:48 +0600

    send data to database using form

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const Users = () => {
-    return (
-        <div>
-            <form>
-                <input type="text" name="name" placeholder='Name...' />
-                <br />
-                <input type="email" name="email" placeholder='email...' />
-                <br />
-                <input type="button" value="Add User" />
-            </form>
-        </div>
-    );
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    // console.log(name, email);
+    const newUser = {name,email}
+
+    // save this user data to the database
+    fetch("http://localhost:3000/users",{
+        method: 'POST',
+        headers:{
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {console.log('after post:', {data})
+    if(data.insertedId){
+        console.log('user added successfully');
+        e.target.reset;
+    }});
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name..." />
+        <br />
+        <input type="email" name="email" placeholder="email..." />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </div>
+  );
 };
 
 export default Users;
